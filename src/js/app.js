a8b42e575e05b1e0c81a2ff7c939af8cd7198343
andrew28092002 <andreiastanaev2002@gmail.com>
2022-12-06 18:35:53 +0500

    add news-block

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,33 @@
-import $ from 'jquery'
-import 'slick-carousel'
+import $ from "jquery";
+import "slick-carousel";
+import Masonry from "masonry-layout";
+import 'imagesloaded'
 
+//Слайдер под шапкой
+$(".top-slider").slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  nextArrow:
+    '<button type="button" class="slick-arrow slick-next"><img src="./images/next-arrow.svg" alt="next"></button>',
+  prevArrow:
+    '<button type="button" class="slick-arrow slick-prev"><img src="./images/prev-arrow.svg" alt="prev"></button>',
+  responsive: [
+    {
+      breakpoint: 640,
+      settings: {
+        arrows: false,
+      },
+    },
+  ],
+});
 
-$('.top-slider').slick({
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="./images/next-arrow.svg" alt="next"></button>',
-    prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="./images/prev-arrow.svg" alt="prev"></button>',
-    responsive: [
-      {
-        breakpoint: 640,
-        settings: {
-          arrows: false,
-        }
-      }
-    ]
+// Таблица masonry для блока "новости"
+const container = document.querySelector(".news__inner");
+
+imagesLoaded(document.querySelector(".news__inner"), () => {
+  const masonry = new Masonry(container, {
+    columnWidth: 270,
+    itemSelector: ".news__item",
   });
+});
